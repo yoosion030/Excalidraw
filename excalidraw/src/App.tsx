f5a111a7ce95b921d9a3822fa8d4da490361cb80
yoosion030 <yoosioff@gmail.com>
2024-03-14 13:23:48 +0900

    feat: excalidraw 라이브러리 추가
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

---
+++ excalidraw/src/App.tsx
@@ -0,0 +1,14 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
+      <div style={{ height: "500px" }}>
+        <Excalidraw />
+      </div>
+    </>
+  );
+}
+
+export default App;
